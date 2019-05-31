1b9811761492b6227bf08ccad94195d3c257c88a
plasmonresonator <mblome@gmail.com>
2019-05-31 10:12:08 -0400

    fixed error where double digits were not handled properly

--- MultiplicationFlashCards_focused._troubleshooting_double_digits.py
+++ MultiplicationFlashCards_focused._troubleshooting_double_digits.py
@@ -33,6 +33,8 @@ level2_base = [0, 1, 2, 4, 5, 10, 11]
 level3_base = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 common_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
+print(f'level1_base {level1_base} level2_base {level2_base} level3_base{level3_base}')
+
 def play_correct_sound():
 	os.chdir(sound_path)
 	winsound.PlaySound('Windows Exclamation.wav', winsound.SND_FILENAME)
@@ -70,76 +72,78 @@ def display_image(image_type, question_number):
 	
 
 def do_level(level, values, common, name, problems=20):
-	try:
-		start1 = datetime.now()
-		starttime1 = start1.strftime('%Y-%m-%d-%H-%M-%S')
-		indiv_path = 'c:\\users\\212330207\\desktop\\mathresultfiles\\'
-		indiv_filename = os.path.join(indiv_path, starttime1 + '.txt')
-		with open(indiv_filename, 'a+') as ff:
-			with open('Maddie multiplaction results.txt', 'a+') as f:
-				print('OK, ' + str.title(name) + ', let\'s do it!!!\n\n')
-				start = datetime.now()
-				starttime = start.strftime('%Y-%m-%d, %H:%M:%S')
-				f.write(f'Level {level} start time: {starttime}\n')
-				ff.write(f'Level {level} start time: {starttime}\n')
-				
-				for i in range(problems):
-					num1 = int(random.choice(values))
-					num2 = int(random.choice(common))
-					answer = num1 * num2
-					problem = 'What is ' + str(num1) + ' x ' + str(num2) + ' ?\t '
-					while True:
-						x = input(problem)
-						try:
-							int(x)
-							break
-						except ValueError:
-							print("\nThat's not a number silly!!! Try typing your answer again!!! :-)")
 
+	print(f'level: {level}\nvalues: {values}\ncommon:{common}\nname: {name}\nproblems: {problems}')
+	# try:
+	start1 = datetime.now()
+	starttime1 = start1.strftime('%Y-%m-%d-%H-%M-%S')
+	indiv_path = 'c:\\users\\212330207\\desktop\\mathresultfiles\\'
+	indiv_filename = os.path.join(indiv_path, starttime1 + '.txt')
+	with open(indiv_filename, 'a+') as ff:
+		with open('Maddie multiplaction results.txt', 'a+') as f:
+			print('OK, ' + str.title(name) + ', let\'s do it!!!\n\n')
+			start = datetime.now()
+			starttime = start.strftime('%Y-%m-%d, %H:%M:%S')
+			f.write(f'Level {level} start time: {starttime}\n')
+			ff.write(f'Level {level} start time: {starttime}\n')
+			
+			for i in range(problems):
+				num1 = int(random.choice(values))
+				num2 = int(random.choice(common))
+				answer = num1 * num2
+				problem = 'What is ' + str(num1) + ' x ' + str(num2) + ' ?\t '
+				while True:
+					x = input(problem)
+					try:
+						int(x)
+						break
+					except ValueError:
+						print("\nThat's not a number silly!!! Try typing your answer again!!! :-)")
+
+				if int(x) == int(answer):
+					print(str(random.choice(right_answer_messages)) + '\n')
+					display_image('correct', i)
+					right.append(i)
+					f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT\n')
+					ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT\n')
+				elif int(x) != int(answer):
+					print('\nOops, you missed it that time.  Let\'s try it one more time!')
+					x = input(problem)
 					if int(x) == int(answer):
 						print(str(random.choice(right_answer_messages)) + '\n')
-						display_image('correct', i)
 						right.append(i)
-						f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT\n')
-						ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT\n')
-					elif int(x) != int(answer):
-						print('\nOops, you missed it that time.  Let\'s try it one more time!')
-						x = input(problem)
-						if int(x) == int(answer):
-							print(str(random.choice(right_answer_messages)) + '\n')
-							right.append(i)
-							f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT (2nd try)\n')
-							ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT (2nd try)\n')
-						else:
-							print(str(random.choice(wrong_answer_messages)) + '\n')
-							print(f'The correct answer was {num1} x {num2} = {answer}')
-							display_image('wrong', i)
-							wrong.append(i)
-							f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  WRONG\n')
-							ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  WRONG\n')
+						f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT (2nd try)\n')
+						ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  CORRECT (2nd try)\n')
+					else:
+						print(str(random.choice(wrong_answer_messages)) + '\n')
+						print(f'The correct answer was {num1} x {num2} = {answer}')
+						display_image('wrong', i)
+						wrong.append(i)
+						f.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  WRONG\n')
+						ff.write(f'Problem {i}: {problem} \tCorrect answer is {answer}.  Your answer was {x}.  WRONG\n')
 
-					total.append(i)
-				end = datetime.now()
-				duration = end - start
-				time_message = '\nTotal time to answer 10 problems: ' + str(duration) + ' seconds.\n'
-				accuracy = 'You answered ' + str(len(right)) + ' out of 10 questions correctly.\n'
-				linebreak = '\n ################################################ \n\n'
+				total.append(i)
+			end = datetime.now()
+			duration = end - start
+			time_message = '\nTotal time to answer 10 problems: ' + str(duration) + ' seconds.\n'
+			accuracy = 'You answered ' + str(len(right)) + ' out of 10 questions correctly.\n'
+			linebreak = '\n ################################################ \n\n'
 
-				print(time_message)
-				print(accuracy)
-				print(linebreak)
-				f.write(time_message)
-				f.write(accuracy)
-				f.write(linebreak)
-				ff.write(time_message)
-				ff.write(accuracy)
-				ff.write(linebreak)
-	except Exception as e:
-		print(f'An error occurred with the message: \n\t{e}\nPlease let Daddy know!')
+			print(time_message)
+			print(accuracy)
+			print(linebreak)
+			f.write(time_message)
+			f.write(accuracy)
+			f.write(linebreak)
+			ff.write(time_message)
+			ff.write(accuracy)
+			ff.write(linebreak)
+	# except Exception as e:
+	# 	print(f'An error occurred with the message: \n\t{e}\nPlease let Daddy know!')
 
 
 print('\n\n')
-name = input('Hello, thanks for wanting to play Daddy\'s super amazing multiplication game!!!\n\nPlease type your name:\t')
+name = 'matt' #input('Hello, thanks for wanting to play Daddy\'s super amazing multiplication game!!!\n\nPlease type your name:\t')
 if str.lower(name) == 'maddie':
 	pass
 else:
@@ -153,9 +157,14 @@ while keep_playing == str.lower('y'):
 	wrong = []
 	total = []
 	while True:
-		focus = input('What number would you like to practice on for this round?\t')
+		focus = 999 #input('What number would you like to practice on for this round?\t')
+		print(focus)
+		focus = int(focus)
+		print(f'focus after storing as int: {focus}')
 		try:
 			int(focus)
+			focus = [focus]
+			print(f'focus list: {focus}')
 			break
 		except ValueError:
 			print("\nThat's not a number silly!!! Try typing the number you want to focus on again!!! :-)")
